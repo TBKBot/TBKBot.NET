197afc0f4d44646171d9871826cbbb9bf1d6897e
mechanira <marklegend293@gmail.com>
2024-03-20 13:58:42 +0200

    deepl auth key

--- Config/JSONReader.cs
+++ Config/JSONReader.cs
@@ -5,8 +5,10 @@ namespace TBKBot
     internal class JSONReader
     {
         public string Token { get; set; }
+        public string DeepLKey { get; set; }
         public string Prefix { get; set; }
-        public ulong? WelcomeChannel {  get; set; }
+        public ulong? WelcomeChannel { get; set; }
+
         public async Task ReadJSON()
         {
             using (StreamReader sr = new StreamReader("config.json"))
@@ -15,6 +17,7 @@ namespace TBKBot
                 JSONStructure data = JsonConvert.DeserializeObject<JSONStructure>(json);
 
                 this.Token = data.Token;
+                this.DeepLKey = data.DeepLKey;
                 this.Prefix = data.Prefix;
                 this.WelcomeChannel = data.WelcomeChannel;
             }
@@ -24,6 +27,7 @@ namespace TBKBot
     internal sealed class JSONStructure
     {
         public string Token { get; set; }
+        public string DeepLKey { get; set; }
         public string Prefix { get; set; }
         public ulong? WelcomeChannel { get; set; }
     }
