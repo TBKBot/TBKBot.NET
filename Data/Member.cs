675191f09a436570b97b3eba3ddde4a82c709407
mechanira <marklegend293@gmail.com>
2024-03-20 13:59:47 +0200

    economy based changes

--- Data/Member.cs
+++ Data/Member.cs
@@ -8,6 +8,9 @@ namespace TBKBot.Models
         public ulong Id { get; set; }
         public string Username { get; set; }
         public int Money { get; set; }
+        public int Bank { get; set; }
         public DateTime? Birthday { get; set; }
+        public DateTime? RespawnTime { get; set; }
+        public DateTime? LastStealTime { get; set; }
     }
 }
